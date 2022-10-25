ea29ac598f10cab894dddea40497c7904433ba10
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-10-25 15:45:22 +0200

    Snack 2 completed!

--- Snack2/main.js
+++ Snack2/main.js
@@ -6,8 +6,8 @@ let countName = name.length;
 let surname = ['Nicoletti','De Lisi','Magris','De Ceglia','Lega','Lukaku']; //Array cognomi
 let countSurname = name.length;
 
-let nameRandom = name[Math.floor(Math.random()* countName)];
-let surnameRandom = surname[Math.floor(Math.random()* countSurname)] ;
+let nameRandom = name[Math.floor(Math.random() * countName)];
+let surnameRandom = surname[Math.floor(Math.random() * countSurname)] ;
 
 console.log(nameRandom + ' ' + surnameRandom); 
 
