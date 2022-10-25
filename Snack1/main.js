228c624504c342dc98f7b67fa893471eef6fb98e
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-10-25 14:28:44 +0200

    Snack 1 completed!

--- Snack1/main.js
+++ Snack1/main.js
@@ -1,2 +1,9 @@
-let nome = "paolo";
-console.log(nome);
+
+
+let number = parseInt(prompt("scrivi un numero")); //Input numero dell'utente
+
+    if(number % 2 === 0) { //Condizione che mi dice se il numero è pari
+        document.getElementById("text").innerHTML = `Il tuo numero è : ${number}` 
+    } else { //Condizione che mi dice se il numero è dispari
+        document.getElementById("text").innerHTML = `Il tuo numero è : ${number +  1}`
+    }
---
+++ Snack4/main.js
@@ -0,0 +1,5 @@
+// let numero = parseInt(prompt('Inserisci un numero'));
+
+let number = parseInt(prompt("scrivi un numero"));
+
+console.log(number);
